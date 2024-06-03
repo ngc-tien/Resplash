03642de370dc51a0dfdee2f98d242a634079bee5
tien.ngc <chitien173@gmail.com>
2024-06-04 06:09:06 +0700

    temp patch safe call api

--- app/src/main/java/com/ngc/tien/resplash/modules/core/BaseRefreshListViewModel.kt
+++ app/src/main/java/com/ngc/tien/resplash/modules/core/BaseRefreshListViewModel.kt
@@ -1,9 +1,11 @@
 package com.ngc.tien.resplash.modules.core
 
 import androidx.lifecycle.LiveData
+import com.ngc.tien.resplash.data.remote.mapper.collection.Collection
+import com.ngc.tien.resplash.util.UiState
 
 interface IBaseRefreshListViewModel {
-    val uiStateLiveData: LiveData<BaseRefreshListUiState>
+    val uiStateLiveData: LiveData<UiState<List<Collection>>>
 
     fun loadFirstPage()
 
